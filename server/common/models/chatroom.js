d7454974e089fed89f15233d8881679e910bbadd
ktk1012 <kimtkyeom@kaist.ac.kr>
2016-07-12 12:06:16 +0000

    Add member, message, and chat room

--- server/common/models/chatroom.js
+++ server/common/models/chatroom.js
@@ -1,6 +1,96 @@
-/**
- * Created by q on 2016-07-11.
- */
-module.exports = function(ChatRoom){
+var loopback = require('loopback');
 
+module.exports = function(ChatRoom){
+  'use strict',
+  ChatRoom.join = function(roomId, ctx, cb) {
+    console.log(ctx.req);
+    if (!ctx.req.accessToken) {
+      console.log("No access token found on request");
+      throw new Error(401, "You need to login");
+    }
+    var userId = ctx.req.accessToken.userId;
+    var Room = {}
+    var query = {
+      id: roomId
+    };
+    ChatRoom.findOne({where: query})
+      .then(function(room) {
+        if (room === undefined) {
+          throw new Error(404, "No room found");
+        }
+        console.log(room)
+        Room = room;
+
+        var query = {
+          id: userId
+        };
+        return ChatRoom.app.models.member.findOne({where: query})
+      }).then(function(member) {
+
+        /* Check user is already in the room */
+        var isNewMember = true;
+        for (var i = 0; i < Room.participant.length; i++) {
+          if (Room.participant[i].id.equals(userId)) {
+            isNewMember = false;
+            break;
+          }
+        }
+
+        /* If not found memeber with id and add it */
+        if(isNewMember) {
+          Room.participant.push(member);
+        }
+        return ChatRoom.upsert(Room);
+      }).then(function(room, err) {
+        if(err) {
+          throw err;
+        }
+        var filter = {
+          limit: 2,
+          order: 'createdOn DESC'
+        };
+        /* To be refactored (add skip to pagenation) */
+        return ChatRoom.app.models.Message.find(filter)
+        // cb(null, room);
+      }).then(function(messages, err) {
+        if(err) {
+          throw err;
+        }
+        console.log(messages);
+        cb(null, messages);
+        /* To be refactored
+         * message type will be
+         * {
+         *   "next": "url to next",
+         *   "data": array of messages"
+         * }*/
+      }).catch(function(err) {
+        cb(err);
+      });
+  };
+
+  ChatRoom.remoteMethod(
+    'join',
+    {
+      description: 'Join into created session',
+      accepts: [
+        {
+          arg: "roomId",
+          type: "string",
+          http: {source: "query"}
+        },
+        {
+          arg: "ctx",
+          type: "object",
+          http: {source: "context"}
+        }
+      ],
+      returns: {
+        arg: 'messages', type: 'object', root: true,
+        description:
+          'The response body contains top 100 messages'
+      },
+      http: {verb: 'get'}
+    }
+  );
 }
--- server/common/models/member.js
+++ server/common/models/member.js
@@ -1,6 +1,10 @@
-/**
- * Created by q on 2016-07-11.
- */
-module.exports = function(Model){
-  
-}
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+var s3 = new AWS.S3();
+
+module.exports = function(Member) {
+  Member.beforeRemote('create', function(ctx, unused, next) {
+    console.log(ctx.res);
+  });
+};
