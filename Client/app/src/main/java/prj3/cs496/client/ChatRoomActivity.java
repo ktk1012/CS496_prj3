d5035e8826f208d024b28332b74007e1ddc6fa9d
hjenie <hjenie06@gmail.com>
2016-07-14 01:55:06 +0900

    Bug fixed at tab B

--- Client/app/src/main/java/prj3/cs496/client/ChatRoomActivity.java
+++ Client/app/src/main/java/prj3/cs496/client/ChatRoomActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.strongloop.android.loopback.RestAdapter;
 import com.strongloop.android.loopback.callbacks.ObjectCallback;
@@ -46,7 +47,7 @@ public class ChatRoomActivity extends AppCompatActivity
             public void onClick(View v) {
                 String chat = chatTxt.getText().toString();
                 if(chat == null && chat.isEmpty()){
-
+                    Toast.makeText(getApplicationContext(),"Empty",Toast.LENGTH_SHORT).show();
                 }else{
 
                 }
