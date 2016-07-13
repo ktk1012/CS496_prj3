14e19552cc34b9b4f60a1d39b25668258e3b3cbc
hjenie <hjenie06@gmail.com>
2016-07-14 07:57:48 +0900

    Restore add_friend and edit addroom

--- Client/app/src/main/java/prj3/cs496/client/AddRoomActivity.java
+++ Client/app/src/main/java/prj3/cs496/client/AddRoomActivity.java
@@ -1,7 +1,9 @@
 package prj3.cs496.client;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AddRoomActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class AddRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_room);
+
+        Button makeRoom = (Button)findViewById(R.id.room_btn);
+        makeRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent intent = new Intent(getApplicationContext(), MediaUpload.class);
+                intent.putExtra("roomId", roomId);
+                startActivity(intent);
+                */
+            }
+        });
     }
 }
